442228d7f2e7b1fe172d4ecbc2669290721c2af8
QuentinHt <huet-quentin@hotmail.fr>
2022-07-21 19:09:34 +0200

    Popin fonctionnel avec titre dynamique

--- src/app/popin-simple/popin-simple.component.ts
+++ src/app/popin-simple/popin-simple.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-popin-simple',
@@ -9,10 +9,20 @@ export class PopinSimpleComponent implements OnInit {
 
   constructor() { }
 
+
+  @Input() title!: string; 
+
   ngOnInit(): void {
   }
 
   closePopin(event: any): void {
     event.target.closest('app-popin-simple').classList.add('hidden');
+  }
+
+  selectNumber(event: any): void {
+    let number = event.target.closest('button').textContent;
+    let appPopin = event.target.closest('app-popin-simple');
+    appPopin.parentNode.firstChild.value = number;
+    appPopin.classList.add('hidden');
   }
 }
