a29de648ae5b46a5face28014224eaf4ffffcb58
QuentinHt <huet-quentin@hotmail.fr>
2022-07-29 16:34:26 +0200

    function button

--- src/app/Component/popin/popin-simples/popin-simples.component.ts
+++ src/app/Component/popin/popin-simples/popin-simples.component.ts
@@ -6,17 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popin-simples.component.less']
 })
 export class PopinSimplesComponent implements OnInit {
+  title: string = '';
 
   constructor() { }
+
+  ctn = document.getElementsByClassName('.clavierTactileSimple');
 
   ngOnInit(): void {
   }
 
   selectNumber(event: any): void {
     let number = event.target.closest('button').textContent;
+    this.title += number; 
+  }
+
+  selectFinalNumber(event: any): void {
+    let number = event.target.closest('button').textContent;
     let popin = event.target.closest('next-popin-simples');
-    popin.parentNode.firstChild.value = number;
-    console.log(popin.parentNode.firstChild)
+    this.title == '' ? this.title = '0' + number : this.title += number;
+    popin.parentNode.firstChild.value = this.title;
     popin.classList.add('hidden');
   }
 }
