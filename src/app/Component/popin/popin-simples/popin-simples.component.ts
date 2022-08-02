95d1d998e7650f2a9a374810352a956d9af613e1
QuentinHt <huet-quentin@hotmail.fr>
2022-08-02 17:10:22 +0200

    css anim button popin

--- src/app/Component/popin/popin-simples/popin-simples.component.ts
+++ src/app/Component/popin/popin-simples/popin-simples.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopinSimplesComponent implements OnInit {
   title: any = '';
-  disabled: any = '';
+  disabled: string = '';
 
   constructor() { }
 
