27a5521f9b04d75811caaf7bef0f862087c5ad49
kenny herillard <k.herillard@cegi.fr>
2022-08-17 10:56:56 +0200

    Add animation on sliding panel

--- src/app/Component/SlidingPanel/sliding-panel/sliding-panel.component.ts
+++ src/app/Component/SlidingPanel/sliding-panel/sliding-panel.component.ts
@@ -1,19 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sliding-panel',
-  templateUrl: './sliding-panel.component.html',
-  styleUrls: ['./sliding-panel.component.less']
+	selector: 'app-sliding-panel',
+	templateUrl: './sliding-panel.component.html',
+	styleUrls: ['./sliding-panel.component.less']
 })
 export class SlidingPanelComponent implements OnInit {
-  @Input() title:string = "Titre du panel";
-  @Input() width: number = 512;
-  isOpen: boolean = false;
-  toolbar: boolean = false;
+	@Input() title: string = "Titre du panel";
+	@Input() width: number = 512;
+	isOpen: boolean = true;
+	openClassName: string = "open";
+	toolbar: boolean = false;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	closePanel(): void {
+		this.openClassName = "";
+		setTimeout(() => {
+			this.isOpen = false;
+		}, 750);
+	}
 }
