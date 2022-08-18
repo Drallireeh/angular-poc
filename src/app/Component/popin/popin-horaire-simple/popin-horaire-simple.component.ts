2808220842b2a0a48bef73878c6ba80a8c5ec211
QuentinHt <huet-quentin@hotmail.fr>
2022-08-18 16:52:49 +0200

    fin popin horaire

--- src/app/Component/popin/popin-horaire-simple/popin-horaire-simple.component.ts
+++ src/app/Component/popin/popin-horaire-simple/popin-horaire-simple.component.ts
@@ -7,16 +7,13 @@ import { Subject } from 'rxjs';
   styleUrls: ['./popin-horaire-simple.component.less']
 })
 export class PopinHoraireSimpleComponent implements OnInit {
-  // Variable contenant le nombre
-  title: any = '';
-  // Variable pour gérer la désactivation de la virgule
-  disabled: string = '';
 
   check: boolean = false;
   click: boolean = false;
 
   minute: string = "00";
   heure: string = "00";
+  title: any = `${this.heure}:${this.minute}`;
 
   // Boolean gérant l'affichage de la popin
   @Input() showPopin: boolean = false;
@@ -36,20 +33,32 @@ export class PopinHoraireSimpleComponent implements OnInit {
 
   // Clic sur un chiffre secondary qui ferme la popin après avoir ajouté au nombre ce qui est présent sur le bouton
   selectFinalNumber(event: any): void {
-    // let number = event.target.closest('button').textContent;
-    // let popin = event.target.closest('next-popin-simples');
-
-    // // Si rien n'est présent dans le nombre, rajoute un 0 devant la virgule
-    // this.title == '' ? this.title = '0' + number : this.title += number;
-    // popin.parentNode.firstChild.value = this.title;
-    // this.changing.next(false);
+    let number = event.target.closest('button').textContent;
+    let popin = event.target.closest('next-popin-horaire-simple');
+    this.title = number
+    popin.parentNode.firstChild.value = this.title;
+    this.changing.next(false);
   }
 
    selectHoraire(): void {
     this.check = true;
-    console.log(this.minute, this.heure)
-    this.title = this.heure + ':' + this.minute
-  }
+  };
+
+  selectHeure(event: any): void {
+    this.check = true;
+    this.heure = event.target.value;
+    this.updateTitle();
+  };
+
+  selectMinute(event: any): void {
+    this.check = true;
+    this.minute = event.target.value;
+    this.updateTitle();
+  };
+
+  updateTitle(): void {
+    this.title = `${this.heure}:${this.minute}`;
+  };
 
   // Fonction de fermeture de la popin
   closePopin(): void {
