03d488867e8806834c9841bce46ae788729b3b2e
QuentinHt <huet-quentin@hotmail.fr>
2022-08-25 15:28:09 +0200

    popin double com

--- src/app/Component/popin/popin-double/popin-double.component.ts
+++ src/app/Component/popin/popin-double/popin-double.component.ts
@@ -10,21 +10,26 @@ import { faEdit } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./popin-double.component.less']
 })
 export class PopinDoubleComponent implements OnInit {
-  // Variable contenant le nombre
+  // Variable contenant le nombre et la mesure
   title: any = '';
+  // Variable qu'on enverra dans l'input
   inputVal: any = '';
   // Variable pour gérer la désactivation de la virgule
   disabled: string = '';
-
+  // Variable contenant les datas que l'utilisateur renseigne
   data: Array<Array<string>> = [['g', 'mg'], ['l', 'ml']];
+  // Récupération des variablse à inscrire dans la popin à l'init
   topValue: string = this.data[0][0];
   botValue: string = this.data[1][0];
-
+  // Variable contenant la mesure en cours
   mesureActuelle: string = this.data[0][0];
+  // Variables pour forcer le focus sur le bouton de mesure sélectionner
   mesure1: string = "forceFocus";
   mesure2: string = "";
   mesure3: string = "";
   mesure4: string = "";
+
+  // Variable contenant l'icône du bouton
   faEdit = faEdit;
 
   // Boolean gérant l'affichage de la popin
@@ -40,7 +45,7 @@ export class PopinDoubleComponent implements OnInit {
     // Fonction pour changer le boolean qui gère l'affichage de la popin
     this.changing.subscribe(v => { 
 			this.showPopin = v;
-		  });
+    });
   }
 
   // Clic sur un chiffre primary pour écrire un nombre
@@ -68,17 +73,20 @@ export class PopinDoubleComponent implements OnInit {
     this.changing.next(false);
   }
 
+  // Clic sur un bouton de valeur préconstruite qui ferme la popin et remplit l'input
   selectDirectValue(event: any): void {
     let number = event.target.closest('button').textContent;
     this.inputVal = number;
     this.changing.next(false);
   }
 
+  // Sélection d'une des mesures de la première ligne sur le clavier tactile
   selectTopMesure(event: any): void {
     this.topValue = event.target.textContent;
     this.mesureActuelle = event.target.textContent;
   }
 
+  // Sélection de la première mesure de la première ligne du clavier tactile
   selectMesure1(): void {
     this.mesure1 = "forceFocus";
     this.mesure2 = "";
@@ -86,6 +94,7 @@ export class PopinDoubleComponent implements OnInit {
     this.mesure4 = "";
   }
 
+  // Sélection de la seconde mesure de la première ligne du clavier tactile
   selectMesure2(): void {
     this.mesure1 = "";
     this.mesure2 = "forceFocus";
@@ -93,11 +102,13 @@ export class PopinDoubleComponent implements OnInit {
     this.mesure4 = "";
   }
 
+  // Sélection d'une des mesures de la seconde ligne sur le clavier tactile
   selectBotMesure(event: any): void {
     this.botValue = event.target.textContent;
     this.mesureActuelle = event.target.textContent;
   }
 
+  // Sélection de la première mesure de la seconde ligne du clavier tactile
   selectMesure3(): void {
     this.mesure1 = "";
     this.mesure2 = "";
@@ -105,6 +116,7 @@ export class PopinDoubleComponent implements OnInit {
     this.mesure4 = "";
   }
 
+  // Sélection de la seconde mesure de la seconde ligne du clavier tactile
   selectMesure4(): void {
     this.mesure1 = "";
     this.mesure2 = "";
@@ -125,11 +137,12 @@ export class PopinDoubleComponent implements OnInit {
     this.changing.next(false);
   }
 
+  // Fonction d'ouverture de la popin
   openPopin(): void {
     this.changing.next(true);
   }
 
-  // Fonction de fermeture de la popin
+  // Fonction de fermeture de la popin et remplissage de l'input
   closePopinWithValue(event:any): void {
     this.inputVal = this.title + this.mesureActuelle;
     this.changing.next(false);
