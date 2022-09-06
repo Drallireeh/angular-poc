637ebe31a9aeee124f1a1b811260e30c2c990528
kenny herillard <k.herillard@cegi.fr>
2022-09-06 12:58:46 +0200

    add comments and prepare update

--- src/app/Component/PartialViews/update-allergie/update-allergie.component.ts
+++ src/app/Component/PartialViews/update-allergie/update-allergie.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { AllergieInterface } from 'src/app/allergie-interface';
 import { BtnAsRadioInterface } from 'src/app/btn-as-radio-interface';
 import { AllergiesService } from 'src/app/services/allergies.service';
@@ -11,6 +11,8 @@ import { LineIdService } from 'src/app/services/line-id.service';
 	styleUrls: ['./update-allergie.component.less']
 })
 export class UpdateAllergieComponent implements OnInit {
+	@Input() updateEvent = new Observable<void>();
+
 	listBtnRadio: BtnAsRadioInterface[] = [{
 		'label': 'Aucune',
 		'selected': true
@@ -23,7 +25,7 @@ export class UpdateAllergieComponent implements OnInit {
 		'label': 'Hypersensibilité',
 		'selected': false
 	}];
-	id?: string;
+	id: string = '';
 	label: string = "";
 	dateValue: Date = new Date(2022, 10, 10);
 	commentary: string = "";
@@ -32,11 +34,27 @@ export class UpdateAllergieComponent implements OnInit {
 	code: string = "";
 	updated_by: string = '';
 
+	// Init allergie
+	allergie: AllergieInterface = {
+		label: '',
+		date: '',
+		code: '',
+		commentaire: '',
+		data_id: '',
+		data_sejour_id: '',
+		type: ''
+	};
+
 	destroy$ = new Subject<void>();
 
 	constructor(private allergieSrv: AllergiesService, private idSrv: LineIdService) { }
 
 	ngOnInit(): void {
+		this.updateEvent.pipe(takeUntil(this.destroy$)).subscribe(() => {
+			// console.log("this.commentary sub : ", this.commentary)
+			this.updateAllergie();
+		});
+		// Ecoute sur la récupération de l'id de la ligne, pour ensuite récupérer l'allergie
 		this.idSrv.lineIdListener().pipe(takeUntil(this.destroy$)).subscribe((elementId) => {
 			if (!elementId) {
 				this.label = "label vide";
@@ -45,6 +63,7 @@ export class UpdateAllergieComponent implements OnInit {
 				this.commentary = "";
 				this.allergieType = "";
 				this.updated_by = '';
+				this.id = '';
 			}
 			else {
 				this.allergieSrv.getAllergie(elementId).subscribe(allergie => {
@@ -53,17 +72,22 @@ export class UpdateAllergieComponent implements OnInit {
 					this.code = allergie.code;
 					this.commentary = allergie.commentaire
 					this.allergieType = allergie?.type;
+					// On récupère l'objet allergie pour changer uniquement les champs qui peuvent être modifiés lors de l'update
+					this.allergie = allergie;
 				});
-				this.getAllergieType();
+				this.setAllergieType();
 			}
 		});
 	}
 
-	getAllergieType(): void {
+	// on set la valeur du type de l'allergie
+	setAllergieType(): void {
+		// Si on a aucune allergie sélectionnée (cas d'ajout d'allergie) alors on sélectionne par défaut le premier type
 		if (!this.allergieType) {
 			this.listBtnRadio.forEach(element => element.selected = false);
 			this.listBtnRadio[0].selected = true;
 		}
+		// Sinon on compare le label de type présent dans l'allergie pour choisir le bon type
 		else {
 			for (let i = 0; i < this.listBtnRadio.length; i++) {
 				if (this.allergieType === this.listBtnRadio[i].label) this.listBtnRadio[i].selected = true;
@@ -76,14 +100,17 @@ export class UpdateAllergieComponent implements OnInit {
 	// 	this.allergieType = label;
 	// }
 
+	// Suppression du code cim10 de l'allergie
 	removeCim10(): void {
 		this.code = '';
 	}
 
+	// Event trigger lors du changement du commentaire
 	changeComment(value: string) {
 		this.commentary = value;
 	}
 
+	// Event trigger lor du changement de la date
 	changeDate(e: Date) {
 		console.log(e)
 	}
@@ -113,5 +140,17 @@ export class UpdateAllergieComponent implements OnInit {
 	}
 
 	updateAllergie(): void {
+		// On attribue toutes les valeurs succeptibles d'avoir changées dans le panel
+		// console.log(this.commentary)
+		// debugger
+		this.allergie.code = this.code;
+		this.allergie.commentaire = this.commentary;
+		this.allergie.date = this.date;
+		this.allergie.updated_by = "Dr Herillard Kenny";
+		this.allergie.type = this.allergieType;
+
+
+		// Appel au service pour faire l'update de notre data
+		this.allergieSrv.updateAllergie(this.allergie);
 	}
 }
