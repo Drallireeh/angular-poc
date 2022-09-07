90da4d40b8d442dcfe8c522af609a67b7f355b8a
QuentinHt <huet-quentin@hotmail.fr>
2022-09-07 17:25:59 +0200

    show hide err input number

--- src/app/Component/next-input-number/next-input-number.component.ts
+++ src/app/Component/next-input-number/next-input-number.component.ts
@@ -10,6 +10,8 @@ export class NextInputNumberComponent implements OnInit {
   constructor() { }
 
   noValid = false; 
+  error = false;
+  errorInput = "Champs mal renseigné";
   @Input() maxValue!:number; 
   @Input() minValue!:number; 
 
@@ -18,12 +20,31 @@ export class NextInputNumberComponent implements OnInit {
 
   validate(event:any) {
     var val: number = +event.target.value
-    if(val > this.maxValue || val < this.minValue){
+    if(val > this.maxValue){
       this.noValid = true;
+      this.errorInput = `La valeur est trop élevée, elle doit être inéferieure à ${this.maxValue}`;
+      this.error = true;
+    }
+    else if(val < this.minValue){
+      this.noValid = true;
+      this.errorInput = `La valeur est trop basse, elle doit être supérieure à ${this.minValue}`;
+      this.error = true;
     }
     else {
       this.noValid = false;
     }
+  }
+
+  showErr(){
+    if(this.noValid){
+      this.error = true;
+    }
+    else {
+      this.error = false
+    }
+  }
+  hideErr(){
+    this.error = false;
   }
   
 }
