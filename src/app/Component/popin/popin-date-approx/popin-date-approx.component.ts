0887786492ee2cf46c5010b1867c3220087180b4
QuentinHt <huet-quentin@hotmail.fr>
2022-09-08 16:52:26 +0200

    Init popin date approx

---
+++ src/app/Component/popin/popin-date-approx/popin-date-approx.component.ts
@@ -0,0 +1,95 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'next-popin-date-approx',
+  templateUrl: './popin-date-approx.component.html',
+  styleUrls: ['./popin-date-approx.component.less']
+})
+export class PopinDateApproxComponent implements OnInit {
+
+  // Variable qui active / désactive la sélection d'horaire manuelle
+  check: boolean = false;
+  // Variable pour définir si l'on peut modifier la checkbox par clic natif
+  click: boolean = false;
+  // Variable des minutes pour le titre
+  minute: string = "00";
+  // Variable des heures pour le titre
+  heure: string = "00";
+  // Variable du formatée du titre
+  title: any = `${this.heure}:${this.minute}`;
+  // Variable qu'on enverra dans l'input
+  inputVal: any = '';
+  // Variable contenant l'icône du bouton
+  faEdit = faEdit;
+
+  // Boolean gérant l'affichage de la popin
+  @Input() showPopin: boolean = false;
+
+  // Subject permettant de gérer le changement de la variable d'affichage de la popin depuis d'autres composants
+  @Input() changing: Subject<boolean> = new Subject();
+  
+  // Boolean gérant l'affichage de la popin
+  @Input() placeholderValue: string = ""; 
+  // Variable du titre du header
+  @Input() titleHeader: string = ""; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    // Fonction pour changer le boolean qui gère l'affichage de la popin
+    this.changing.subscribe(v => { 
+			this.showPopin = v;
+		  });
+  }
+
+  // Clic sur une case horaire préremplie et fermeture de la popin + remplissage de l'input
+  selectFinalNumber(event: any): void {
+    let number = event.target.closest('button').textContent;
+    this.title = number;
+    this.inputVal = number;
+    this.changing.next(false);
+  }
+
+  // Clic sur la ligne de la checkbox pour passer en saisie manuelle
+   selectHoraire(): void {
+    this.check = true;
+  };
+
+  // Changement de l'heure manuellement
+  selectHeure(event: any): void {
+    this.check = true;
+    this.heure = event.target.value;
+    this.updateTitle();
+  };
+
+  // Changement des minutes manuellement
+  selectMinute(event: any): void {
+    this.check = true;
+    this.minute = event.target.value;
+    this.updateTitle();
+  };
+
+  // Reformate le titre
+  updateTitle(): void {
+    this.title = `${this.heure}:${this.minute}`;
+  };
+
+    // Fonction d'ouverture de la popin
+    openPopin(): void {
+      this.changing.next(true);
+    }
+
+  // Fonction de fermeture de la popin
+  closePopin(): void {
+    this.changing.next(false);
+  }
+
+  // Fonction de fermeture de la popin et remplissage de l'input
+  closePopinWithValue(event:any): void {
+    this.inputVal = this.title;
+    this.changing.next(false);
+    } 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,6 +55,7 @@ import { NextCalendarComponent } from './Component/next-calendar/next-calendar.c
 import { SearchAllergiesComponent } from './Component/Views/search-allergies/search-allergies.component';
 import { PopinDatepickerComponent } from './Component/popin/popin-datepicker/popin-datepicker.component';
 import { NextInputNumberComponent } from './Component/next-input-number/next-input-number.component';
+import { PopinDateApproxComponent } from './Component/popin/popin-date-approx/popin-date-approx.component';
 
 @NgModule({
   declarations: [
@@ -87,6 +88,7 @@ import { NextInputNumberComponent } from './Component/next-input-number/next-inp
     SearchAllergiesComponent,
     PopinDatepickerComponent,
     NextInputNumberComponent,
+    PopinDateApproxComponent,
   ],
   imports: [
     BrowserModule,
