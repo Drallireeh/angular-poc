a2aa5c8ce9528407b617b94877f821f1b638d087
QuentinHt <huet-quentin@hotmail.fr>
2022-09-08 15:31:05 +0200

    annule la valeur si erreur

--- src/app/Component/next-input-number/next-input-number.component.ts
+++ src/app/Component/next-input-number/next-input-number.component.ts
@@ -12,6 +12,7 @@ export class NextInputNumberComponent implements OnInit {
   noValid = false; 
   error = false;
   errorInput = "Champs mal renseigné";
+  valInput = 0;
   @Input() maxValue!:number; 
   @Input() minValue!:number; 
   @Output() changeVal = new EventEmitter();
@@ -21,19 +22,20 @@ export class NextInputNumberComponent implements OnInit {
 
   validate(event:any) {
     var val: number = +event.target.value;
-    this.changeVal.emit(val);
     if(val > this.maxValue){
       this.noValid = true;
-      this.errorInput = `La valeur est trop élevée, elle doit être inéferieure à ${this.maxValue}`;
+      this.errorInput = `La valeur est trop élevée, elle doit être inéferieure ou égale à ${this.maxValue}`;
       this.error = true;
     }
     else if(val < this.minValue){
       this.noValid = true;
-      this.errorInput = `La valeur est trop basse, elle doit être supérieure à ${this.minValue}`;
+      this.errorInput = `La valeur est trop basse, elle doit être supérieure ou égale à ${this.minValue}`;
       this.error = true;
     }
     else {
       this.noValid = false;
+      this.valInput = val;
+      this.changeVal.emit(this.valInput);
     }
   }
 
