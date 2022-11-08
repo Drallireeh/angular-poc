dc74573bae89c1beb5dc620ab8fca2f217d1555c
kenny herillard <k.herillard@cegi.fr>
2022-11-08 12:11:02 +0100

    Add selection of allergies to add

--- src/app/Component/PartialViews/liste-med-to-add-allergie/liste-med-to-add-allergie.component.ts
+++ src/app/Component/PartialViews/liste-med-to-add-allergie/liste-med-to-add-allergie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { THRESHOLD_DIFF } from '@progress/kendo-angular-popup/services/scrollable.service';
 import { MedicamentAllergie } from 'src/app/medicament-allergie-interface';
 
 @Component({
@@ -9,33 +10,44 @@ import { MedicamentAllergie } from 'src/app/medicament-allergie-interface';
 export class ListeMedToAddAllergieComponent implements OnInit {
 	@Input() listeMedicaments: MedicamentAllergie[] = [
 		{
+			id: 1,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 2,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 3,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 4,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 5,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 6,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 7,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 8,
 			label: 'AAZ COVID-VIRO test antigénique'
 		},
 		{
+			id: 9,
 			label: 'AAZ COVID-VIRO test antigénique'
 		}
 	]
+
+	listeMedSelected: MedicamentAllergie[] = [];
 
 	constructor() { }
 
@@ -43,8 +55,15 @@ export class ListeMedToAddAllergieComponent implements OnInit {
 	}
 
 	selectLine(medicament: any) {
-		console.log(medicament)
-		medicament.selected = !medicament.selected
-		// TODO set med selected
+		medicament.selected = !medicament.selected;
+
+		let indexMed = this.listeMedSelected.findIndex(med => med.id === medicament.id);
+		if (medicament.selected) {
+			this.listeMedSelected.push(medicament);
+		} else {
+			this.listeMedSelected.splice(indexMed, 1);
+		}
+
+		console.log(this.listeMedSelected)
 	}
 }
